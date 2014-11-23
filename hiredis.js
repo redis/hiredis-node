b951d47f6693bb42bc6eceb9f135182918f36178
Stéphan Kochen <stephan@kochen.nl>
2014-11-23 22:04:57 +0100

    Extract command writer into separate export.

--- hiredis.js
+++ hiredis.js
@@ -2,18 +2,24 @@ var net = require("net"),
     hiredis = require('bindings')('hiredis.node');
 
 exports.Reader = hiredis.Reader;
+
+exports.writeCommand = function() {
+    var i, args = arguments;
+    var str = "*" + args.length + "\r\n";
+    for (i = 0; i < args.length; i++) {
+        var arg = args[i];
+        str += "$" + arg.length + "\r\n" + arg + "\r\n";
+    }
+    return str;
+}
+
 exports.createConnection = function(port, host) {
     var s = net.createConnection(port || 6379, host);
     var r = new hiredis.Reader();
     var _write = s.write;
 
     s.write = function() {
-        var i, args = arguments;
-        var str = "*" + args.length + "\r\n";
-        for (i = 0; i < args.length; i++) {
-            var arg = args[i];
-            str += "$" + arg.length + "\r\n" + arg + "\r\n";
-        }
+        var str = exports.writeCommand.apply(this, arguments);
         return _write.call(s, str);
     }
 
