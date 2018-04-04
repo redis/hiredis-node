4bb094bd5e091210fc028938740a7e5bb5a9778d
NickNaso <nicoladelgobbo@gmail.com>
2018-04-05 01:53:26 +0200

    Deleted unuseful  file used on development phase

--- raw.js
+++
@@ -1,9 +0,0 @@
-var hiredis = require('bindings')('hiredis')
-
-var reader = new hiredis.Reader();
-
-test("NullBulkReply", function() {
-    var reader = new hiredis.Reader();
-    reader.feed("$-1\r\n");
-    assert.equal(null, reader.get());
-});
